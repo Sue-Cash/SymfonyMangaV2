96f0022788756a032738454f70655c8bb661aa93
Misha <misharahnema@gmail.com>
2025-04-12 13:38:54 +0200

    Ajout de Category

--- app/src/Controller/DashboardController.php
+++ app/src/Controller/DashboardController.php
@@ -15,11 +15,11 @@ class DashboardController extends AbstractController
     public function index(Request $request, MangaRepository $mangaRepository): Response
     {
         $title    = $request->query->get('title');         // recherche par titre
-        $priceParam    = $request->query->get('price');    // Récupère la chaîne de caractère
+        $priceParam = $request->query->get('price');         // Récupère la chaîne de caractère
         // Convertir la chaîne vide en null sinon convertir en float
         $price    = ($priceParam !== '' && $priceParam !== null) ? (float)$priceParam : null;
-        $author   = $request->query->get('author');        // recherche par auteur
-        $onlyMine = $request->query->getBoolean('onlyMine'); // checkbox pour filtrer mes mangas
+        $author   = $request->query->get('author');         // recherche par auteur
+        $onlyMine = $request->query->getBoolean('onlyMine');  // checkbox pour filtrer mes mangas
 
         $userId = $onlyMine ? $this->getUser()->getId() : null;
 
